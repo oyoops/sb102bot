921aa4774c81fd9f0df87581549b991693aa0d32
oyoops <121578862+oyoops@users.noreply.github.com>
2023-09-21 23:42:17 -0400

    Update location.py

--- location.py
+++ location.py
@@ -70,3 +70,14 @@ class Location:
         logging.debug(f"Extracted City: {city}, Extracted County: {county}")
 
         return city, county
+
+@staticmethod
+def haversine_distance(lat1, lon1, lat2, lon2):
+    R = 6371  # Radius of Earth in kilometers
+    dlat = math.radians(lat2 - lat1)
+    dlon = math.radians(lon2 - lon1)
+    a = math.sin(dlat / 2) * math.sin(dlat / 2) + math.cos(math.radians(lat1)) \
+        * math.cos(math.radians(lat2)) * math.sin(dlon / 2) * math.sin(dlon / 2)
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+    distance = R * c
+    return distance
